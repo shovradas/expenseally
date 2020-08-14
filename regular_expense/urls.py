9e2817b9fbe38f12163cb36df1ff6548cbf094b7
Shovra <shovradas@gmail.com>
2020-08-14 18:16:58 +0200

    project structure revised

--- regular_expense/urls.py
+++ regular_expense/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add-expense', views.add_expense, name='add-expense'),
+    path('manage-expense', views.manage_expense, name='manage-expense'),
+    path('expense-settings', views.expense_settings, name='expense-settings'),
 ]
--- regular_expense/views.py
+++ regular_expense/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
 def index(request):
     view_data = {
         'page_title': 'Dashboard',
-        'page_header': 'Dashboard'
+        'page_header': 'Your Dashboard'
     }
     return render(request, 'regular_expense/index.html', view_data)
+
+
+def add_expense(request):
+    view_data = {
+        'page_title': 'Add Expense',
+        'page_header': 'Expense | Add New'
+    }
+    return render(request, 'regular_expense/add-expense.html', view_data)
+
+
+def manage_expense(request):
+    view_data = {
+        'page_title': 'Manage Expense',
+        'page_header': 'Expense | Manage'
+    }
+    return render(request, 'regular_expense/manage-expense.html', view_data)
+
+
+def expense_settings(request):
+    view_data = {
+        'page_title': 'Expense Settings',
+        'page_header': 'Expense | Settings'
+    }
+    return render(request, 'regular_expense/expense-settings.html', view_data)
+
+
+# def about(request):
+#     developedBy = "Shovra, Rumi, Jahangir"
+#
+#     html = '<!DOCTYPE html>'
+#     html += '<html>'
+#     html += '<body>'
+#     html += 'About ExpenseAlly'
+#     html += '<br/>Developed by: '
+#     html += developedBy
+#     html += '<body>'
+#     html += '</body>'
+#     html += '</html>'
+#     return HttpResponse(html)
+
+# def about(request):
+#     model = {
+#         "developed_by": "Shovra, Rumi, Jahangir"
+#     }
+#     return render(request, 'about.html', model)
